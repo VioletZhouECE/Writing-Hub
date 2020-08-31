eb7184cbe1ca81c72004cae199744db03eea8474
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-08-31 10:14:55 +0800

    init language and user models

---
+++ app/backend/models/languages.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const Language = sequelize.define('Language', {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false
+      },
+
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.fn('NOW'),
+        allowNull: false
+      },
+
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.fn('NOW'),
+        allowNull: false
+      },
+
+      languageName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    });
+  
+    Language.associate = models => {
+      Language.belongsToMany(models.User, {through: "FirstLanguageUsers", as: "FirstLanguageUsers"});
+      Language.belongsToMany(models.User, {through: "LearnLanguageUsers", as: "LearnLanguageUsers"});
+    };
+  
+    return Language;
+  };
---
+++ app/backend/models/users.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false
+      },
+
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.fn('NOW'),
+        allowNull: false
+      },
+
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.fn('NOW'),
+        allowNull: false
+      },
+
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      
+      points: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 0
+      }
+    });
+  
+    User.associate = models => {
+        User.belongsToMany(models.Language, {through: "FirstLanguageUsers", as: "FirstLanguageUsers"});
+        User.belongsToMany(models.Language, {through: "LearnLanguageUsers", as: "LearnLanguageUsers"});
+    };
+  
+    return User;
+  };
