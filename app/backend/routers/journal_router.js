4c91a54f86a41a7f41cee43392de5f400b42fd1e
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-09-04 09:07:03 +0800

    modify journal route,  hardcoded language tabs

--- app/backend/routers/journal_router.js
+++ app/backend/routers/journal_router.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const {getJournals, getJournal, postJournal, updateJournal, deleteJournal} = require('../controllers/journals_controller');
 
-//GET /journals
-router.get('/', getJournals);
+//GET /journals/all/langauge/:languageName
+router.get('/all/user/:userId', getJournals);
+
+//GET /journals/all/langauge/:languageName
+router.get('/all/language/:languageName', getJournals);
 
 //GET /journals/:journalId
 router.get('/:journalId', getJournal);
--- app/frontend/static/components/home/home_page.js
+++ app/frontend/static/components/home/home_page.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+const languages = ["Simplified Chinese", "English", "Japanese"];
+
 class HomePage extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
     render(){
+        let tabs = [];
+        tabs.push(<div className = "tab-float-left"><a href="">Feed</a></div>);
+        languages.forEach(element => {
+            tabs.push( <div className = "tab-float-left"><a href="">{element}</a></div>);
+        });
+        tabs.push(<div className = "tab-float-left"><a href="">...</a></div>);
+
         return(
-            <div>Home Page</div>
+            <div className = "center-container">
+                {tabs}
+            </div>
         )
     }
 }
