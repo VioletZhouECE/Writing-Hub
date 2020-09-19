4c6bcfe3d9b4e614e4f73426a30e3f617bc6a954
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-09-20 07:11:40 +0800

    make displayed comment scrollable

--- app/frontend/static/components/home/selected_comment.js
+++ app/frontend/static/components/home/selected_comment.js
@@ -7,14 +7,16 @@ class SelectedComment extends React.Component{
 
     render(){
         return (
-            <div className="form-group displayed-comment-box">
-                <span className="float-left mr-2"><i className="fas fa-user-circle fa-2x"></i></span>
-                <div className="float-left mb-2">
-                    <div className="comment-username">{this.props.selectedComment.author}</div>
-                    <div className="comment-time">Placeholder for time</div>
+            <div className="form-group">
+                <div className="form-control displayed-comment-box">
+                    <span className="float-left mr-2"><i className="fas fa-user-circle fa-2x"></i></span>
+                    <div className="float-left mb-2">
+                        <div className="comment-username">{this.props.selectedComment.author}</div>
+                        <div className="comment-time">Placeholder for time</div>
+                    </div>
+                    <div className="clear-float"></div>
+                    <div>{this.props.selectedComment.comment}</div>
                 </div>
-                <div className="clear-float"></div>
-                <div>{this.props.selectedComment.comment}</div>
             </div>
         )
     }
