b5ea55ab660aa8dd3a042e7ad4a5c1e2e655a9ac
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-09-21 02:55:18 +0800

    add styling to home page language bar

--- app/frontend/static/components/home/home_page.js
+++ app/frontend/static/components/home/home_page.js
@@ -91,18 +91,34 @@ class HomePage extends React.Component{
         }, this.loadPost);
     }
 
+    // <div className = "tab-float-left"><a href="#" onClick = {(e) => this.loadFeed(e.target.value)}>Feed</a></div>
+    // <div className = "tab-float-left" key={this.props.learnLanguage}><a href="javascript:;" onClick = {(e) => this.switchLanguage(this.props.learnLanguage)}>{this.props.learnLanguage}</a></div>
+    // <div className = "tab-float-left" key={this.props.firstLanguage}><a href="javascript:;" onClick = {(e) => this.switchLanguage(this.props.firstLanguage)}>{this.props.firstLanguage}</a></div>
+    // <div className = "tab-float-left"><a href="#">...</a></div>
+    // <div className="clear-float"></div>
+
     render(){
         return(
             <div>
-                <div className="tab-bar">
-                    <div className = "tab-float-left"><a href="#" onClick = {(e) => this.loadFeed(e.target.value)}>Feed</a></div>
-                    <div className = "tab-float-left" key={this.props.learnLanguage}><a href="javascript:;" onClick = {(e) => this.switchLanguage(this.props.learnLanguage)}>{this.props.learnLanguage}</a></div>
-                    <div className = "tab-float-left" key={this.props.firstLanguage}><a href="javascript:;" onClick = {(e) => this.switchLanguage(this.props.firstLanguage)}>{this.props.firstLanguage}</a></div>
-                    <div className = "tab-float-left"><a href="#">...</a></div>
-                    <div className="clear-float"></div>
+                <div className= "tab-bar mt-4">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="javascript:;" onClick = {(e) => this.loadFeed(e.target.value)}>Feed</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="javascript:;" key={this.props.learnLanguage} onClick = {(e) => this.switchLanguage(this.props.learnLanguage)}>{this.props.learnLanguage}</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="javascript:;" key={this.props.firstLanguage} onClick = {(e) => this.switchLanguage(this.props.firstLanguage)}>{this.props.firstLanguage}</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="javascript:;">...</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-                <br></br>
-                <div className = "center-container">
+                <div className = "center-container mt-3">
                     {this.state.posts.map(post => (<PostSummary id={post.id} key={post.id} username={post.username} title={post.title} body={post.body} viewsCount={post.viewsCount}></PostSummary>))}
                     <div ref={loadingRef => (this.loadingRef = loadingRef)}></div>
                     <div className = "text-center" style={{display: this.state.isLoading? "block" : "none"}}> Loading... </div>
