a8d7151046eb17b8919aa382966dc5acf04cc548
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-09-21 01:24:25 +0800

    add styling to menu bar

--- app/frontend/static/components/home/post_summary.js
+++ app/frontend/static/components/home/post_summary.js
@@ -6,10 +6,10 @@ class PostSummary extends React.Component{
         return (
             <div className = "pt-2">
                 <div className = "d-flex justify-content-between">
-                    <div className = "p-0 col-sm-3 col-md-3 col-lg-2">
-                        {this.props.username}
+                    <div className = "p-0 col-sm-2 col-md-2 col-lg-2">
+                        <i className="fas fa-user-circle fa-3x"></i>
                     </div>
-                    <div className = "pl-2 col-sm-9 col-md-9 col-lg-10 d-flex flex-column">
+                    <div className = "pl-2 col-sm-10 col-md-10 col-lg-10 d-flex flex-column">
                         <Link to={`/journal/${this.props.id}`}>
                             <div className = "post-title-wrap font-weight-bold pb-3">
                                 {this.props.title}
--- app/frontend/static/components/reusable/menu_bar.js
+++ app/frontend/static/components/reusable/menu_bar.js
@@ -1,23 +1,40 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-class MenuBar extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-        <div style = {{backgroundColor: "rgb(52, 225, 235)"}}>
-            <div className = "d-flex flex-row justify-content-center">
-                <div className = "col-sm-2 col-md-2 col-lg-2"><Link to = "/">Home</Link></div>
-                <div className = "col-sm-2 col-md-2 col-lg-2"><Link >Entries</Link></div>
-                <div className = "col-sm-2 col-md-2 col-lg-2"><Link>Profile</Link></div>
-                <div className = "col-sm-2 col-md-2 col-lg-2" onClick = {this.props.handleLogout}><Link>Logout</Link></div>
-                <div className = "col-sm-2 col-md-2 col-lg-2"><Link to = "/write">Write</Link></div>
-            </div>
+const MenuBar = (props)=>{
+    return(
+        <div className="bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark main-container">
+                <a className="navbar-brand" href="#">Writing Hub</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="javascript:;"><Link to = "/">Home</Link></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="javascript:;"><Link >Entries</Link></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="javascript:;"><Link>Profile</Link></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="javascript:;"><Link>Logout</Link></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="javascript:;"><Link to = "/write">Write</Link></a>
+                    </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0 float-right">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                    <button className="btn btn-primary my-2 my-sm-0" type="submit">Start Writing!</button>
+                </form>
+                </div>
+            </nav>
         </div>
-        )
-    }
+    )
 }
 
 export default MenuBar;
