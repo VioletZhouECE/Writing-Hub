4f92bf892e2793b5c671461d51000c6ea41e5ba2
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-09-21 02:09:20 +0800

    change menubar color to lightblue

--- app/frontend/static/components/reusable/menu_bar.js
+++ app/frontend/static/components/reusable/menu_bar.js
@@ -1,35 +1,39 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const MenuBar = (props)=>{
+    const history = useHistory();
+
+    const handleClickButton = ()=>{
+        history.push("/write");
+    }
+
     return(
-        <div className="bg-dark">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark main-container">
-                <a className="navbar-brand" href="#">Writing Hub</a>
+        <div className="menubar-background">
+            <nav className="navbar navbar-expand-lg navbar-light main-container menubar-background">
+                <a className="navbar-brand" href="javascript:;">Writing Hub</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="javascript:;"><Link to = "/">Home</Link></a>
+                        <a className="nav-link" href="/">Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="javascript:;"><Link >Entries</Link></a>
+                        <a className="nav-link" href="javascript:;">Entries</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="javascript:;"><Link>Profile</Link></a>
+                        <a className="nav-link" href="javascript:;">Profile</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="javascript:;"><Link>Logout</Link></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="javascript:;"><Link to = "/write">Write</Link></a>
+                        <a className="nav-link" href="javascript:;" onClick = {props.handleLogout}>Logout</a>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0 float-right">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button className="btn btn-primary my-2 my-sm-0" type="submit">Start Writing!</button>
+                    <i className="fas fa-user-circle fa-2x mr-2"></i>
+                    <button className="btn btn-primary my-2 my-sm-0" type="submit" onClick={handleClickButton}>Start Writing!</button>
                 </form>
                 </div>
             </nav>
