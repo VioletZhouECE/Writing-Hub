738a2f388817e88746d30989fe477511cf57297d
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-11-05 19:23:01 -0500

    fix bug with frontend editComment & refactor classes

--- app/frontend/static/components/home/comment.js
+++ app/frontend/static/components/home/comment.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Editbox from "./editbox"
 
 class Comment extends React.Component{
     constructor(props){
@@ -11,19 +12,26 @@ class Comment extends React.Component{
         }
     }
 
-    //enter editing mode
     handleClickEdit(){
         this.setState({isEditing:true});
         this.currentCommentBox.current.style.height = "165px";
     }
 
-    //exit editing mode
     handleSaveCancel(){
         this.setState({
-            isEditing: false,
-            comment: this.props.commentInfo.content
+            isEditing: false
         });
-        this.currentCommentBox.current.style.height = "86px"
+        this.currentCommentBox.current.style.height = "86px";
+    }
+
+    handleSaveComment(newComment){
+        this.setState({
+            isEditing: false,
+            comment: newComment
+        });
+
+        //actually save the comment 
+        this.currentCommentBox.current.style.height = "86px";
     }
 
     handleCommentChange(e){
@@ -37,24 +45,18 @@ class Comment extends React.Component{
                     <span className="float-left mr-2"><i className="fas fa-user-circle fa-2x"></i></span>
                     <div className="float-left mb-2">
                         <div className="comment-username float-left">{this.props.commentInfo.author}</div>
-                        <div className="float-right">
-                            <i className="fas fa-ellipsis-h" href="#" role="button" data-toggle="dropdown"></i>
-                                <div className="dropdown-menu dropdown-menu-left">
-                                    <a href="#" className="dropdown-item" onClick={this.handleClickEdit.bind(this)}>Edit</a>
-                                    <a href="#" className="dropdown-item">Delete</a>
-                                </div> 
-                        </div>
                         <div className="clear-float"></div>
                         <div id={this.props.commentId} className="comment-time">{this.props.commentInfo.time}</div>
                     </div>
-                    <div className="clear-float"></div>
-                    {this.state.isEditing? <textarea className="form-control mb-2" rows="2" value={this.state.comment} onChange = {this.handleCommentChange.bind(this)}></textarea> : <div>{this.props.commentInfo.content}</div>}
-                    <div style={{display: this.state.isEditing? "block": "none"}}>
-                        <div className = "mt-2">
-                            <button type="button" className="btn btn-sm btn-primary mb-1 float-right" role="button">Save</button>
-                            <button type="button" className="btn btn-sm btn-secondary mb-1 float-right mr-2" role="button" onClick={this.handleSaveCancel.bind(this)}>Cancel</button>
-                        </div>
+                    <div className="float-right">
+                        <i className="fas fa-ellipsis-h" href="#" role="button" data-toggle="dropdown"></i>
+                            <div className="dropdown-menu dropdown-menu-left">
+                                <a href="#" className="dropdown-item" onClick={this.handleClickEdit.bind(this)}>Edit</a>
+                                <a href="#" className="dropdown-item">Delete</a>
+                            </div> 
                     </div>
+                    <div className="clear-float"></div>
+                    {this.state.isEditing? <Editbox comment = {this.state.comment} handleSaveComment={this.handleSaveComment.bind(this)} handleSaveCancel={this.handleSaveCancel.bind(this)}></Editbox> : <div>{this.state.comment}</div>}
                     <div className="clear-float"></div>
                 </div>
             </div>
---
+++ app/frontend/static/components/home/editbox.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class Editbox extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            comment: this.props.comment
+        }
+    }
+
+    handleCommentChange(e){
+        this.setState({comment: e.target.value});
+    }
+
+    render(){
+        return (
+            <>
+                <textarea className="form-control mb-2" rows="2" value={this.state.comment} 
+                onChange = {this.handleCommentChange.bind(this)}></textarea>
+                <div className = "mt-2">
+                    <button type="button" className="btn btn-sm btn-primary mb-1 float-right" role="button" onClick={() => this.props.handleSaveComment(this.state.comment)}>Save</button>
+                    <button type="button" className="btn btn-sm btn-secondary mb-1 float-right mr-2" role="button" onClick={this.props.handleSaveCancel}>Cancel</button>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Editbox
