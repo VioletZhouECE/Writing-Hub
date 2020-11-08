d3d19fc6f13bb67197903df1b480b5aa1d91de9b
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-11-08 09:59:11 -0500

    final fix to comment display

--- app/frontend/static/components/home/comment.js
+++ app/frontend/static/components/home/comment.js
@@ -46,19 +46,22 @@ class Comment extends React.Component{
     render(){
         return (
             <div className="form-group position-relative">
-                <div className="form-control displayed-comment-box" ref={this.currentCommentBox}>
+                <div className="form-control displayed-comment-box position-relative" ref={this.currentCommentBox}>
                     <span className="float-left mr-2"><i className="fas fa-user-circle fa-2x"></i></span>
-                    <div className="float-left mb-2" style={{width: "80%"}}>
-                        <div className="comment-username float-left">{this.props.commentInfo.author}</div>
-                        {this.UserInfo.username == this.props.commentInfo.author && <div className="editMenu">
-                        <div className="float-right">
-                            <i className="fas fa-ellipsis-h" href="#" role="button" data-toggle="dropdown"></i>
-                                <div className="dropdown-menu dropdown-menu-left">
-                                    <a href="#" className="dropdown-item" onClick={this.handleClickEdit.bind(this)}>Edit</a>
-                                    <a href="#" className="dropdown-item">Delete</a>
-                                </div> 
-                            </div>
-                        </div>}
+                    <div className ="float-left">
+                        <div className="float-left comment-username">
+                            {this.props.commentInfo.author}
+                        </div>
+                        {this.UserInfo.username == this.props.commentInfo.author && 
+                            <div className = "editmenu">
+                                <div className="float-right">
+                                    <i className="fas fa-ellipsis-h" href="#" role="button" data-toggle="dropdown"></i>
+                                        <div className="dropdown-menu dropdown-menu-left">
+                                            <a href="#" className="dropdown-item" onClick={this.handleClickEdit.bind(this)}>Edit</a>
+                                            <a href="#" className="dropdown-item">Delete</a>
+                                        </div> 
+                                </div>
+                            </div>}
                         <div className="clear-float"></div>
                         <div id={this.props.commentId} className="comment-time">{this.props.commentInfo.time}</div>
                     </div>
