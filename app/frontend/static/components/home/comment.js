4bcdbf87c822ed26f4584aba46d37c55e4bc5b9c
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-11-07 22:24:58 -0500

    fix comment display

--- app/frontend/static/components/home/comment.js
+++ app/frontend/static/components/home/comment.js
@@ -1,6 +1,8 @@
 import React from "react";
 import CommentEdit from "./comment_edit"
 
+{/* <div id={this.props.commentId} className="comment-time">{this.props.commentInfo.time}</div> */}
+
 class Comment extends React.Component{
     constructor(props){
         super(props);
@@ -46,20 +48,20 @@ class Comment extends React.Component{
             <div className="form-group position-relative">
                 <div className="form-control displayed-comment-box" ref={this.currentCommentBox}>
                     <span className="float-left mr-2"><i className="fas fa-user-circle fa-2x"></i></span>
-                    <div className="float-left mb-2">
+                    <div className="float-left mb-2" style={{width: "80%"}}>
                         <div className="comment-username float-left">{this.props.commentInfo.author}</div>
-                        <div className="clear-float"></div>
-                        <div id={this.props.commentId} className="comment-time">{this.props.commentInfo.time}</div>
-                    </div>
-                    {this.UserInfo.username == this.props.commentInfo.author && <div className="editMenu">
+                        {this.UserInfo.username == this.props.commentInfo.author && <div className="editMenu">
                         <div className="float-right">
                             <i className="fas fa-ellipsis-h" href="#" role="button" data-toggle="dropdown"></i>
                                 <div className="dropdown-menu dropdown-menu-left">
                                     <a href="#" className="dropdown-item" onClick={this.handleClickEdit.bind(this)}>Edit</a>
                                     <a href="#" className="dropdown-item">Delete</a>
                                 </div> 
-                        </div>
-                    </div>}
+                            </div>
+                        </div>}
+                        <div className="clear-float"></div>
+                        <div id={this.props.commentId} className="comment-time">{this.props.commentInfo.time}</div>
+                    </div>
                     <div className="clear-float"></div>
                     {this.state.isEditing? <CommentEdit comment = {this.state.comment} handleSaveComment={this.handleSaveComment.bind(this)} handleSaveCancel={this.handleSaveCancel.bind(this)}></CommentEdit> : <div>{this.state.comment}</div>}
                     <div className="clear-float"></div>
