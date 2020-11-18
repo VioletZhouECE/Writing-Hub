f66ddd55c8e5c177f5eb196deeb2c8e6e8ca0e5a
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-11-17 22:37:32 -0500

    add process.env variables and connection strings

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -1,6 +1,19 @@
-require('dotenv').config();
+let config;
 
-let config = {
+if (process.env.NODE_ENV == "production"){
+  config = {
+    "username": process.env.SQLAZURECONNSTR_DB_USER,
+    "password": process.env.SQLAZURECONNSTR_DB_PASS,
+    "database": "writing-hub-server",
+    "host": process.env.SQLAZURECONNSTR_DB_HOST, 
+    "dialect": "mssql",
+    "dialectOptions" : {
+      "encrypt": true
+    }
+  }
+} else {
+  require('dotenv').config();
+  config = {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": "writing-hub-server",
@@ -9,6 +22,7 @@ let config = {
     "dialectOptions" : {
       "encrypt": true
     }
+  }
 }
 
 module.exports = config;
