e4c04efe88fb6bed75ab23053a8af3614dc01da8
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-11-22 20:20:47 -0500

    fix import issue in config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,12 +1,9 @@
-const config_dev = require('./config_dev');
-const config_prod= require('./config_prod');
-
 let config; 
 
 if (process.env.NODE_ENV == "production"){
-    config = config_prod;
+    config = require('./config_prod');
 } else {
-    config = config_dev;
+    config = require('./config_dev');
 }
 
 module.exports = config;
