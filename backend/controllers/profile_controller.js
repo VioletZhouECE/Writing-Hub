1a05eacd1e868802b9fbf241c0363a550aab32ba
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-11-29 12:12:02 -0500

    fix broken image by excluding image header and adding blob contentType

--- backend/controllers/profile_controller.js
+++ backend/controllers/profile_controller.js
@@ -1,23 +1,27 @@
 const uuid = require('uuid');
+const getStream = require('into-stream');
+const {BlobServiceClient} = require('@azure/storage-blob');
 const {storage} = require('../config/config');
-const azure = require('azure-storage');
 
 //delete the existing avatar (if exists) and store the new avatar
 exports.updateAvatar = async (req, res, next) => {
     try{
-        var blobService = azure.createBlobService();
-        const blobName = uuid.v4();
-        await blobService.createBlockBlobFromText(storage.avatarContainer, 
-            blobName,
-            req.body.image,
-            {contentType: 'image/jpeg', contentEncoding: 'base64'}, 
-            (error, result, response) => {
-                if (error) {
-                throw (error);
-                }
-                console.log("result", result);
-                console.log("response", response);
-            });
+        const blobName = uuid.v4() + '.jpg';
+        //split the image data header
+        const imgBase64 = req.body.image.split(';base64,').pop();
+        //decode base 64 string to buffer
+        const buff = new Buffer(imgBase64, 'base64');
+        //convert buffer to stream
+        const stream = getStream(buff);
+        
+        const blobServiceClient = BlobServiceClient.fromConnectionString(storage.connectionString);
+        const containerClient = blobServiceClient.getContainerClient(storage.avatarContainer);
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+        await blockBlobClient.uploadStream(stream,
+            undefined, undefined,
+            { blobHTTPHeaders: { blobContentType: "image/jpg" } });
+
 
         const response = {msg: "image upload succeeded"}
         res.status(200).json(response);
